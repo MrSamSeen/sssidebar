c9efe58cced7de22e8c009dee264d5b8c95c42ee
Sameera Mallawaarachchi <dmsameeram@gmail.com>
2024-07-09 09:15:16 +0530

    v2

--- sidepanel.js
+++ sidepanel.js
@@ -1,46 +1,65 @@
-// Add your sidebar functionality here
 console.log("Sidebar loaded");
 document.addEventListener("DOMContentLoaded", function () {
-    //cange ifram url when cliked on the span to gemini or chatgpt
+
     document.getElementById("gemini").addEventListener("click", function () {
-        document.getElementById("container").innerHTML = `
-        <iframe class="iframe" src="https://gemini.google.com" allowfullscreen></iframe>
-        `;
-        setContainer();
+        let iframe = document.getElementById("cnt-gemini");
+        if (iframe.innerHTML === "") {
+            iframe.innerHTML = `
+            <iframe class="iframe" src="https://gemini.google.com" allowfullscreen></iframe>
+            `;
+        }
+        setContainer("cnt-gemini");
     })
     document.getElementById("chatgpt").addEventListener("click", function () {
-        document.getElementById("container").innerHTML = `
-        <iframe class="iframe" src="https://chatgpt.com/" allowfullscreen></iframe>
-        `;
-        setContainer();
+        let iframe = document.getElementById("cnt-chatgpt");
+        if (iframe.innerHTML === "") {
+            iframe.innerHTML = `
+            <iframe class="iframe" src="https://chatgpt.com/" allowfullscreen></iframe>
+            `;
+        }
+        setContainer("cnt-chatgpt");
     });
 
-    //load calculator
     document.getElementById("calc").addEventListener("click", function () {
-        document.getElementById("container").innerHTML = `
-        <iframe class="iframe" src="https://calcwidget.netlify.app" allowfullscreen></iframe>
-        `;
-        setContainer();
+        let iframe = document.getElementById("cnt-calc");
+        if (iframe.innerHTML === "") {
+            iframe.innerHTML = `
+            <iframe class="iframe" src="https://calcwidget.netlify.app" allowfullscreen></iframe>
+            `;
+        }
+        setContainer("cnt-calc");
     })
-    //load calendar
+
     document.getElementById("cal").addEventListener("click", function () {
-        document.getElementById("container").innerHTML = `
-        <iframe class="iframe" src="https://calendarwidget.netlify.app" allowfullscreen></iframe>
-        `;
-        setContainer(); // Ensure this function is defined
-    });
+        let iframe = document.getElementById("cnt-cal");
+        if (iframe.innerHTML === "") {
+            iframe.innerHTML = `
+            <iframe class="iframe" src="https://calendarwidget.netlify.app" allowfullscreen></iframe>
+            `;
+        }
+        setContainer("cnt-cal");
+    })
     document.getElementById("notes").addEventListener("click", function () {
-        document.getElementById("container").innerHTML = `
-        <iframe class="iframe" src="https://ssnotes.netlify.app/" allowfullscreen></iframe>
-        `;
-        setContainer(); // Ensure this function is defined
+        let iframe = document.getElementById("cnt-notes");
+        if (iframe.innerHTML === "") {
+            iframe.innerHTML = `
+            <iframe class="iframe" src="https://ssnotes.netlify.app/" allowfullscreen></iframe>
+            `;
+        }
+        setContainer("cnt-notes");
     });
-
-
 });
 
-function setContainer() {
+function setContainer(element) {
+    let content = document.getElementsByClassName("content");
+    for (let i = 0; i < content.length; i++) {
+        content[i].style.display = "none";
+    }
+    console.log(element);
+    document.getElementById(element).style.display = "block";
+    document.getElementById("menu").style.flexWrap = "wrap";
+    document.getElementById("menu").style.flexGrow = "1";
+    document.getElementById("menu").style.flexDirection = "row";
     document.getElementById("container").style.display = "block";
-    //set container hight to 100vh
-    document.getElementById("container").style.height = "92vh";
+    document.getElementById("container").style.height = "98vh";
 }
